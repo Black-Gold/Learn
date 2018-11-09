99d943ff453152e23b0094219a9c9d82b80bc05d
black-gold <crisis2015128@163.com>
2018-11-09 11:21:59 +0800

    all

---
+++ LearnPython3/GrabNovel.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+'''
+从https://www.biqukan.com网站抓取小说
+
+
+'''
---
+++ LearnShell/shelltest.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ --unity-launch --user-data-dir=/root/.config/Code/ --extensions-dir=/root/公共的/extensions/ -r %F
+
+  --no-sandbox --incognito  --disable-infobars 
